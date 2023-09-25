e4321ef0acaa38affc8c78bcef89553cf2a1927b
Mars, like the planet <56095669+magdaouk@users.noreply.github.com>
2023-09-25 08:58:22 -0700

    Add files via upload

---
+++ minNumber.js
@@ -0,0 +1,9 @@
+function find_min (nums) {
+  let min_num = Number.INFINITY; // bigger than all other numbers
+  for (let num of nums) {
+    if (num < min_num) {
+    // (Fill in the missing line here)
+    return min_num;
+    }
+  }
+}
