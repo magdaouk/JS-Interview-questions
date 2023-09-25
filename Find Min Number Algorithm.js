0e7a918a009017d709352600ed1ae3b7da899e60
Mars, like the planet <56095669+magdaouk@users.noreply.github.com>
2023-09-25 08:57:55 -0700

    Delete Find Min Number Algorithm.js

--- Find Min Number Algorithm.js
+++
@@ -1,7 +0,0 @@
-
-function find_min (nums) {
-let min_num = Number. INFINITY, // bigger than all other numbers
-for (let num of nums) {
-if (num < min_num) {
-/ (Fill in the missing line here)
-return min_num;
